ba089efa10f5fa148cc6b9ed4e82f35b35a841ff
Asik-ali <asik14923@gmail.com>
2024-01-01 20:10:56 +0530

    A

--- src/Components/Hero/Hero2.jsx
+++ src/Components/Hero/Hero2.jsx
@@ -4,7 +4,7 @@ import img from '../../assets/3.jpg';
 function Hero2() {
   return (
     <>
-      <div className='bg-[#6062DC] lg:pt-10 pb-10 grid grid-cols-1 md:grid-cols-2 place-items-center relative'>
+      <div className='bg-[#1b2945] lg:pt-10 pb-10 grid grid-cols-1 md:grid-cols-2 place-items-center relative'>
         {/* <div className='w-full lg:w-[800px] relative'>
           <div className="absolute top-0 w-72 h-72 bg-[#4F6F52] rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
           <div className="absolute top-0 right-0 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
@@ -17,7 +17,7 @@ function Hero2() {
 
           <h1 className='text-white text-xl  font-recursive lg:ps-3 lg:pe-3'>
             Start your journey with Link’s Institute by exploring our diverse
-            range of tutoring programs.           <span className='text-[#FBCB1D] lg:pe-3'> Browse through subjects, grade levels and available tutors to find
+            range of tutoring programs.           <span className='text-[#6062DC] lg:pe-3'> Browse through subjects, grade levels and available tutors to find
               the perfect fit for your academic needs.✨  </span> 
           </h1>
           <div className='flex flex-col md:flex-row px-4 pb-4 gap-5 md:ms-4 mt-4'>
