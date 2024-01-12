cea024eaae0a5e2ea86de4d314fa8773c8709396
Musthafa <musthafa2303@gmail.com>
2024-01-12 21:49:48 +0530

    n

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,19 +5,19 @@ export default () => {
             label: "Company",
             items: [
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'About'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Blog'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Team'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Careers'
                 },
             ],
@@ -26,19 +26,19 @@ export default () => {
             label: "Resources",
             items: [
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'contact'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Support'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Docs'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Pricing'
                 },
             ],
@@ -47,19 +47,19 @@ export default () => {
             label: "About",
             items: [
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Terms'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'License'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'Privacy'
                 },
                 {
-                    href: 'javascript:void()',
+                 
                     name: 'About US'
                 },
             ]
@@ -184,28 +184,28 @@ transformative educational experience that goes beyond traditional classroom lea
 
 //     const footerNavs = [
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: 'About'
 //         },
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: 'Blog'
 //         },
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: ''
 //         },
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: 'Team'
 //         },
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: 'Careers'
 //         },
 
 //         {
-//             href: 'javascript:void()',
+//          
 //             name: 'Suuport'
 //         }
 //     ]
