b6a80d12de20fa717a87bb9a19586a57bdd928ac
Musthafa <musthafa2303@gmail.com>
2024-01-14 23:29:25 +0530

    changes

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -5,31 +5,31 @@ import { FaRegUserCircle } from "react-icons/fa";
 function Support() {
     return (
         <div className='bg-white pb-10 overflow-x-hidden'>
-            <h1 className='pt-16 mx-auto text-4xl font-semibold lg:w-[450px] text-center'><span className='text-[#E7A93C]'>BrightFuture </span> Champions</h1>
+            <h1 className='pt-16 mx-auto text-4xl font-semibold  text-center'><span className='text-[#E7A93C]'>BrightFuture </span> Champions</h1>
             <div className='flex flex-col gap-10 lg:flex-row lg:gap-0 justify-evenly items-center  mt-10'>
                 <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#FECC49] rounded-xl h-[50px] w-[120px] ms-2 flex justify-center items-center'>
+                    <div className='bg-[#FECC49] rounded-xl h-[50px] w-[100px] ms-2 flex justify-center items-center'>
                         <MdDesignServices className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Qualified Tutors</h5>
-                        <p className='text-[12px] text-gray-600'>Our team of dedicated tutors consists of experienced professionals and subject matter experts who
+                        <p className='text-[12px] text-gray-600'>Our team of dedicated tutors consists of experienced professionals  experts who
                             are passionate about education.</p>
                     </div>
                 </div>
-                <div className='w-[320px] rounded h-[110px] bg-[#E7E5FD] flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#B0ACFF] rounded h-[60px] w-[120px] ms-2 flex justify-center items-center'>
-                        <FaDollarSign className='text-white text-4xl' />
+                <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
+                    <div className='bg-[#FECC49] rounded-xl h-[50px] w-[100px] ms-2 flex justify-center items-center'>
+                        <FaDollarSign className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Our Approach</h5>
-                        <p className='text-[12px] text-gray-600'>LINKS isn't just about tutoring. We mix supe­rior academics with a positive, uplifting atmosphere­. This helps nurture self-assurance­ and a passion for learning.
+                        <p className='text-[12px] text-gray-600'>LINKS isn't just about tutoring. We mix supe­rior academics with a positive, uplifting atmosphere­. 
 </p>
                     </div>
                 </div>
-                <div className='w-[320px] rounded h-[110px] bg-[#FFDEE9] flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#FF80AB] rounded h-[60px] w-[120px] ms-2 flex justify-center items-center'>
-                        <FaRegUserCircle className='text-white text-4xl' />
+                <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
+                    <div className='bg-[#FECC49] rounded-xl h-[50px] w-[100px] ms-2 flex justify-center items-center'>
+                        <FaRegUserCircle className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Explore Our Programs:</h5>
@@ -39,9 +39,9 @@ range of tutoring programs. </p>
                 </div>
             </div>
             <div className='flex flex-col gap-10 lg:flex-row lg:gap-0 justify-evenly items-center mt-10 px-5'>
-                <div className='w-[320px] rounded h-[110px] bg-[#FFDEE9] flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#FF80AB] rounded h-[60px] w-[120px] ms-2 flex justify-center items-center'>
-                        <FaRegUserCircle className='text-white text-4xl' />
+                <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
+                    <div className='bg-[#FECC49] rounded-xl h-[50px] w-[100px] ms-2 flex justify-center items-center'>
+                        <FaRegUserCircle className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Innovative Environment:</h5>
@@ -49,24 +49,23 @@ range of tutoring programs. </p>
 enhance the learning experience.</p>
                     </div>
                 </div>
-                <div className='w-[320px] rounded h-[110px] bg-[#FDF5E0] flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#FECC49] rounded h-[60px] w-[120px] ms-2 flex justify-center items-center'>
-                        <MdDesignServices className='text-white text-4xl' />
+                <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
+                    <div className='bg-[#FECC49] rounded h-[60px] w-[100px] ms-2 flex justify-center items-center'>
+                        <MdDesignServices className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Collaborative Culture</h5>
                         <p className='text-[12px] text-gray-600'>We believe in the power of collaboration. Our team works together
-to achieve common goals, and we celebrate individual and collective successes.</p>
+to achieve common goals</p>
                     </div>
                 </div>
-                <div className='w-[320px] rounded h-[110px] bg-[#FDF5E0] flex justify-evenly items-center gap-5'>
-                    <div className='bg-[#FECC49] rounded h-[60px] w-[120px] ms-2 flex justify-center items-center'>
-                        <FaDollarSign className='text-white text-4xl' />
+                <div className='w-[370px] rounded-xl border-gray-200 h-[100px] bg-white border-2  flex justify-evenly items-center gap-5'>
+                    <div className='bg-[#FECC49] rounded h-[60px] w-[100px] ms-2 flex justify-center items-center'>
+                        <FaDollarSign className='text-white text-3xl' />
                     </div>
                     <div>
                         <h5 className='font-semibold'>Cutting-Edge Technology</h5>
-                        <p className='text-[12px] text-gray-600'>Utilizing state-of-the-art online learning platforms, we create engaging and interactive sessions that
-enhance the learning experience.</p>
+                        <p className='text-[12px] text-gray-600'>Utilizing state-of-the-art online learning platforms, we create engaging .</p>
                     </div>
                 </div>
 
